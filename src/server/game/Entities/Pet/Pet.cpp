ef69306b52c71c833b08bb5bf20c6dc7200c1618
TheSnowcraft <30834419+TheSnowcraft@users.noreply.github.com>
2026-09-04 22:21:32 +0100

    Update Pet.cpp
    Changes levels at which pet talent points unlock.

--- src/server/game/Entities/Pet/Pet.cpp
+++ src/server/game/Entities/Pet/Pet.cpp
@@ -1798,7 +1798,7 @@ void Pet::InitTalentForLevel()
 
 uint8 Pet::GetMaxTalentPointsForLevel(uint8 level) const
 {
-    uint8 points = (level >= 20) ? ((level - 16) / 4) : 0;
+    uint8 points = (level >= 15) ? ((level - 12) / 3) : 0;
     // Mod points from owner SPELL_AURA_MOD_PET_TALENT_POINTS
     points += GetOwner()->GetTotalAuraModifier(SPELL_AURA_MOD_PET_TALENT_POINTS);
     return points;
