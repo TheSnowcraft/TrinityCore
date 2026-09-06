77934fcb1bea3f503aa2b7195a25d2a4f7156250
TheSnowcraft <30834419+TheSnowcraft@users.noreply.github.com>
2026-09-06 16:04:21 +0100

    Extend Taming the Beast scripts for added hunter races.

--- src/server/scripts/Spells/spell_quest.cpp
+++ src/server/scripts/Spells/spell_quest.cpp
@@ -64,7 +64,13 @@ enum TamingTheBeast
     SPELL_TAME_NIGHTSTALKER_1          = 30652,
     SPELL_TAME_CRAZED_DRAGONHAWK_1     = 30100,
     SPELL_TAME_ELDER_SPRINGPAW_1       = 30103,
-    SPELL_TAME_MISTBAT_1               = 30104
+    SPELL_TAME_MISTBAT_1               = 30104,
+    SPELL_TAME_STONETUSK_BOAR_1        = 100000,
+    SPELL_TAME_GREY_FOREST_WOLF_1      = 100002,
+    SPELL_TAME_YOUNG_FOREST_BEAR_1     = 100004,
+    SPELL_TAME_DECREPIT_DARK_HOUND_1   = 100006,
+    SPELL_TAME_GREATER_DUSKBAT_1       = 100008,
+    SPELL_TAME_TIRISFAL_BEAR_1         = 100010
 };
 
 class spell_quest_taming_the_beast : public AuraScript
@@ -92,7 +98,13 @@ class spell_quest_taming_the_beast : public AuraScript
             SPELL_TAME_NIGHTSTALKER_1,
             SPELL_TAME_CRAZED_DRAGONHAWK_1,
             SPELL_TAME_ELDER_SPRINGPAW_1,
-            SPELL_TAME_MISTBAT_1
+            SPELL_TAME_MISTBAT_1,
+            SPELL_TAME_STONETUSK_BOAR_1,
+            SPELL_TAME_GREY_FOREST_WOLF_1,
+            SPELL_TAME_YOUNG_FOREST_BEAR_1,
+            SPELL_TAME_DECREPIT_DARK_HOUND_1,
+            SPELL_TAME_GREATER_DUSKBAT_1,
+            SPELL_TAME_TIRISFAL_BEAR_1
         });
     }
 
@@ -125,6 +137,12 @@ class spell_quest_taming_the_beast : public AuraScript
             case SPELL_TAME_CRAZED_DRAGONHAWK:     finalSpellId = SPELL_TAME_CRAZED_DRAGONHAWK_1; break;
             case SPELL_TAME_ELDER_SPRINGPAW:       finalSpellId = SPELL_TAME_ELDER_SPRINGPAW_1; break;
             case SPELL_TAME_MISTBAT:               finalSpellId = SPELL_TAME_MISTBAT_1; break;
+            case SPELL_TAME_STONETUSK_BOAR:        finalSpellId = SPELL_TAME_STONETUSK_BOAR_1; break;
+            case SPELL_TAME_GREY_FOREST_WOLF:      finalSpellId = SPELL_TAME_GREY_FOREST_WOLF_1; break;
+            case SPELL_TAME_YOUNG_FOREST_BEAR:     finalSpellId = SPELL_TAME_YOUNG_FOREST_BEAR_1; break;
+            case SPELL_TAME_DECREPIT_DARK_HOUND:   finalSpellId = SPELL_TAME_DECREPIT_DARK_HOUND_1; break;
+            case SPELL_TAME_GREATER_DUSKBAT:       finalSpellId = SPELL_TAME_GREATER_DUSKBAT_1; break;
+            case SPELL_TAME_TIRISFAL_BEAR:         finalSpellId = SPELL_TAME_TIRISFAL_BEAR_1; break;
         }
 
         if (finalSpellId)
